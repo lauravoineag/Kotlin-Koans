6bba12286152985254acc18dc3b9361f5cb31710
Laura Voineag <laura.voineag@bbc.co.uk>
2023-05-15 15:28:59 +0100

    Collections

--- Collections/Filter map/src/Task.kt
+++ Collections/Filter map/src/Task.kt
@@ -1,6 +1,6 @@
 // Find all the different cities the customers are from
 fun Shop.getCustomerCities(): Set<City> =
-        TODO()
+        customers.
 
 // Find the customers living in a given city
 fun Shop.getCustomersFrom(city: City): List<Customer> =
